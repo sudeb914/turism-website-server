026ade9c83ef7a2eaea5c295119da61ffc3c8be3
unknown <sudebkarmokar8@gmail.com>
2021-11-08 16:33:55 +0600

    done

--- index.js
+++ index.js
@@ -21,6 +21,15 @@ async function run() {
         const serviceCollection = database.collection('services');
         const bookingRequestCollection = database.collection('bookingRequests')
 
+
+        app.get('/services/:id',async(req,res)=>{
+            const id = req.params.id;
+            console.log(id);
+            const cursor = await serviceCollection.findOne({_id:ObjectId(id)});
+            console.log(cursor);
+            res.send(cursor);
+
+        })
         // GET services API 
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find({});
